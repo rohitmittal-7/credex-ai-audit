0c805a85d9d2289e66c590197cdd9bd0d1bdc200
Rohit Mittal <rohitmittalrm1000@gmail.com>
2026-05-13 23:47:52 +0530

    fix build issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Home";
-import Results from "./Results";
-import PublicAudit from "./PublicAudit";
+import Home from "./pages/Home";
+import Results from "./pages/Results";
+import PublicAudit from "./pages/PublicAudit";
 
 function App() {
   return (
